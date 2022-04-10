9cd81c56b234740822cf2123412cfca30f968355
Pedro Santayana <pedrosantayana@gmail.com>
2022-04-10 15:49:00 -0300

    comentários exercício 3

--- lista5/exercicio3.c
+++ lista5/exercicio3.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 
 int divisao(int numerador, int denominador) {
+    // Caso base
+    // Retorna 0 pois não é mais possível subtrair sem que o quociente fique negativo 
     if(numerador-denominador < 0) return 0;
+    // Passo recursivo 
+    // Retorna a próxima subtração somado a 1, já que num-den >= 0 
     else return divisao(numerador-denominador, denominador) + 1;
 }
 
