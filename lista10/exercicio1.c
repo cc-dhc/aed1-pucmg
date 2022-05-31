9e4a019fe431ca138e4b92496fa0818172e7b89b
Pedro Santayana <pedrosantayana@gmail.com>
2022-05-30 22:17:10 -0300

    lista 10 ex1 e ex2

--- lista10/exercicio1.c
+++ lista10/exercicio1.c
@@ -5,26 +5,23 @@ int main() {
     FILE *alunos = fopen("alunos.txt", "r");
     FILE *medias = fopen("medias.txt", "w");
 
-    while(feof(alunos)) {
+    while(!feof(alunos)) {
         char nome[50], sobrenome[50];
         int matricula;
         float media=-1;
         fscanf(alunos, "%s %s %d\n", nome, sobrenome, &matricula);
 
         FILE *notas = fopen("notas.txt", "r");
-        while(feof(notas)) {
+        while(!feof(notas)) {
             int mat;
-            float nota1, nota2;
-            fscanf(notas, "%d %f %f\n", &mat, &nota1, &nota2);
+            float n[4], total=0;
+            fscanf(notas, "%d %f %f %f %f\n", &mat, &n[0], &n[1], &n[2], &n[3]);
             if(mat == matricula) {
-                media = (nota1+nota2)/2;
+                for(int i=0; i<4; total+=n[i++]);
+                fprintf(medias, "%s %s %.2f\n", nome, sobrenome, total/4);
                 break;
             }
         }
-        if(media == -1) {
-            continue;
-        }
-        fprintf(medias, "%s %s %.2f\n", nome, sobrenome, media);
     }
     fclose(medias);
 }
---
+++ lista10/exercicio2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int conta_linhas(char *caminho) {
+    FILE *arquivo = fopen(caminho, "r");
+    
+}
+
+int main() {
+    char caminho[100];
+    
+    scanf("%s", caminho);
+
+
+
+}
